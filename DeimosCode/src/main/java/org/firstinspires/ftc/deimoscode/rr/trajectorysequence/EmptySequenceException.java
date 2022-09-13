ac29054089d70fb5ae7935901bd485bb7eba67bb
isaki-na <isara.esc.5@gmail.com>
2022-09-13 17:09:44 -0600

    separate two modules for two teams and add rr for both

---
+++ DeimosCode/src/main/java/org/firstinspires/ftc/deimoscode/rr/trajectorysequence/EmptySequenceException.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.deimoscode.rr.trajectorysequence;
+
+
+public class EmptySequenceException extends RuntimeException { }
---
+++ PhobosCode/src/main/java/org/firstinspires/ftc/phoboscode/rr/trajectorysequence/EmptySequenceException.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.phoboscode.rr.trajectorysequence;
+
+
+public class EmptySequenceException extends RuntimeException { }
