190b5afbe520e067afdaa466f42cc49d207514f3
= <serivesmejia@gmail.com>
2022-10-15 19:05:54 -0600

    meepmeep

---
+++ MeepMeep/src/main/java/com/example/meepmeep/Phobos.kt
@@ -0,0 +1,57 @@
+package com.example.meepmeep
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.geometry.Vector2d
+import com.noahbres.meepmeep.MeepMeep
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder
+
+fun main() {
+    val meepMeep = MeepMeep(600)
+
+    // Declare our first bot
+    val myFirstBot = DefaultBotBuilder(meepMeep) // We set this bot to be blue
+            .setColorScheme(ColorSchemeBlueDark())
+            .setConstraints(60.0, 60.0, Math.toRadians(180.0), Math.toRadians(180.0), 17.5)
+            .followTrajectorySequence { drive ->
+                drive.trajectorySequenceBuilder(Pose2d(-35.0, -58.0, Math.toRadians(90.0))).apply {
+                    lineToConstantHeading(Vector2d(-35.0, -23.0))
+                    waitSeconds(1.0)
+                    splineToSplineHeading(Pose2d(-52.0, -12.5, Math.toRadians(180.0)), Math.toRadians(178.0))
+
+                    waitSeconds(1.2)
+
+                    repeat(4) {
+                        lineToConstantHeading(Vector2d(-24.0, -11.5))
+                        waitSeconds(1.2)
+                        lineToConstantHeading(Vector2d(-54.0, -12.5))
+                        waitSeconds(1.2)
+                    }
+                }.build()
+            }
+
+    // Declare out second bot
+    val mySecondBot = DefaultBotBuilder(meepMeep) // We set this bot to be red
+            .setColorScheme(ColorSchemeRedDark())
+            .setConstraints(60.0, 60.0, Math.toRadians(180.0), Math.toRadians(180.0), 18.0)
+            .followTrajectorySequence { drive ->
+                drive.trajectorySequenceBuilder(Pose2d(30.0, 30.0, Math.toRadians(180.0)))
+                        .forward(30.0)
+                        .turn(Math.toRadians(90.0))
+                        .forward(30.0)
+                        .turn(Math.toRadians(90.0))
+                        .forward(30.0)
+                        .turn(Math.toRadians(90.0))
+                        .forward(30.0)
+                        .turn(Math.toRadians(90.0))
+                        .build()
+            }
+
+    meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_DARK)
+            .setDarkMode(true)
+            .setBackgroundAlpha(0.95f) // Add both of our declared bot entities
+            .addEntity(myFirstBot)
+            .addEntity(mySecondBot)
+            .start()
+}
